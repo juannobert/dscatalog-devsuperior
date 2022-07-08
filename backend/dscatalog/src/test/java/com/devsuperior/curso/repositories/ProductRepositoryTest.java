9040373e34b1bb98f3a4d77239b95a40062cc8e7
Juan Nobert <nobert407@gmail.com>
2022-07-08 10:45:38 -0300

    Product find by id test

--- backend/dscatalog/src/test/java/com/devsuperior/curso/repositories/ProductRepositoryTest.java
+++ backend/dscatalog/src/test/java/com/devsuperior/curso/repositories/ProductRepositoryTest.java
@@ -1,6 +1,6 @@
 package com.devsuperior.curso.repositories;
 
- import java.util.Optional;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,56 +14,63 @@ import com.devsuperior.curso.tests.factory.ProductFactory;
 
 @DataJpaTest
 public class ProductRepositoryTest {
-	
+
 	@Autowired
 	private ProductRepository productRepository;
-	
+
 	private long exintingId;
 	private long nonExistingId;
 	private long countTotalProducts;
-	
+
 	@BeforeEach
-	void setUp() throws Exception{
+	void setUp() throws Exception {
 		exintingId = 1L;
 		nonExistingId = 100L;
 		countTotalProducts = 25L;
 	}
-	
+
 	@Test
 	public void deleteShouldDeleteObjectWhenIdExists() {
-		
+
 		productRepository.deleteById(exintingId);
 		Optional<Product> obj = productRepository.findById(exintingId);
-		
+
 		Assertions.assertFalse(obj.isPresent());
 	}
-	
+
 	@Test
 	public void deleteShouldThrowEmptyResultDataAccessExceptionWhenDoesNotIdExist() {
-		Assertions.assertThrows(EmptyResultDataAccessException.class, () ->{
+		Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
 			productRepository.deleteById(nonExistingId);
 		});
 	}
-	
+
 	@Test
-	public void saveShouldPersistWithAutoIncrementWhenIdIsNUll() {
+	public void saveShouldPersistWithAutoIncrementWhenIdIsNull() {
 		Product product = ProductFactory.createProduct();
-		
+
 		product.setId(null);
 		product = productRepository.save(product);
-		
+
 		Assertions.assertNotNull(product.getId());
-		Assertions.assertEquals(countTotalProducts+1, product.getId());
+		Assertions.assertEquals(countTotalProducts + 1, product.getId());
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+	@Test
+	public void findByIdShouldReturnNonEmptyOptionalWhenIdExist() {
+
+		Optional<Product> obj = productRepository.findById(exintingId);
+
+		Assertions.assertTrue(obj.isPresent());
+
+	}
+
+	@Test
+	public void findByIdShouldReturnEmptyOptionalWhenIdNotExist() {
+
+		Optional<Product> obj = productRepository.findById(nonExistingId);
+
+		Assertions.assertFalse(obj.isPresent());
+	}
+
 }
