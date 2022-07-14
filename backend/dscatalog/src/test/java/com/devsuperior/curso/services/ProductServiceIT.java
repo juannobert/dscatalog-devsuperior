dc9462c709d4fb6821d3cfc3e96545c207f3bfae
Juan Nobert <nobert407@gmail.com>
2022-07-14 10:06:38 -0300

    Product service integration test delete

---
+++ backend/dscatalog/src/test/java/com/devsuperior/curso/services/ProductServiceIT.java
@@ -0,0 +1,46 @@
+package com.devsuperior.curso.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.devsuperior.curso.repositories.ProductRepository;
+import com.devsuperior.curso.services.exceptions.ResourceNotFoundException;
+
+@SpringBootTest
+public class ProductServiceIT {
+
+	@Autowired
+	private ProductService service;
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	private Long existingId;
+	private Long nonExistingId;
+	private Long countTotalProducts;
+	
+	@BeforeEach
+	public void setUp() throws Exception{
+		existingId =1L;
+		nonExistingId = 100L;
+		countTotalProducts = 25L;
+	}
+	
+	@Test
+	public void deleteShouldDeleteResourceWhenIdExists() {
+		service.delete(existingId);
+		
+		Assertions.assertEquals(countTotalProducts - 1, repository.count());
+	}
+	
+	@Test
+	public void deleteShouldThrowResourceNotFoundExcepionWhenIdDoesNotExist() {
+		Assertions.assertThrows(ResourceNotFoundException.class,() ->{
+			service.delete(nonExistingId);
+		});
+	}
+	
+}
