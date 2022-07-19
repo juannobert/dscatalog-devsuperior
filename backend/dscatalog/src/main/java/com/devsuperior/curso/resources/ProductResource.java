0b048c98abae6cf2f6a64c3f62e691bb6002a86d
Juan Nobert <nobert407@gmail.com>
2022-07-19 19:49:41 -0300

    Custom validation response

--- backend/dscatalog/src/main/java/com/devsuperior/curso/resources/ProductResource.java
+++ backend/dscatalog/src/main/java/com/devsuperior/curso/resources/ProductResource.java
@@ -2,6 +2,8 @@ package com.devsuperior.curso.resources;
 
 import java.net.URI;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -38,7 +40,7 @@ public class ProductResource {
 	}
 	
 	@PostMapping
-	public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto){
+	public ResponseEntity<ProductDTO> insert(@RequestBody @Valid ProductDTO dto){
 		dto = service.insert(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(dto.getId()).toUri();
@@ -47,7 +49,7 @@ public class ProductResource {
 	}
 	
 	@PutMapping("/{id}")
-	public ResponseEntity<ProductDTO> update(@PathVariable Long id,@RequestBody ProductDTO dto){
+	public ResponseEntity<ProductDTO> update(@PathVariable Long id,@RequestBody @Valid ProductDTO dto){
 		dto = service.update(id,dto);
 		return ResponseEntity.ok(dto);
 		
