edc41b42d75a3283aa27c95add30ddfded1a0987
Aluno <aluno@ifslab.edu.br>
2022-07-27 11:06:51 -0300

    Resource server

--- backend/dscatalog/src/main/java/com/devsuperior/curso/config/SecurityConfiguration.java
+++ backend/dscatalog/src/main/java/com/devsuperior/curso/config/SecurityConfiguration.java
@@ -4,11 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfiguration {
 	
 
