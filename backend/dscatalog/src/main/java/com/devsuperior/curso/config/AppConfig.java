f93687d495197f73825c57562d09612ab4b1cd0d
Juan Nobert <nobert407@gmail.com>
2022-08-19 16:21:03 -0300

    Profile dev and conection with postgreesql

--- backend/dscatalog/src/main/java/com/devsuperior/curso/config/AppConfig.java
+++ backend/dscatalog/src/main/java/com/devsuperior/curso/config/AppConfig.java
@@ -3,6 +3,8 @@ package com.devsuperior.curso.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
@@ -24,6 +26,12 @@ public class AppConfig {
     	tokenConverter.setSigningKey(jwtSecret);
     	return tokenConverter;
     }
+    
+    @Bean
+	AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+		return authConfig.getAuthenticationManager();
+	}
+
 
     @Bean
     JwtTokenStore tokenStore() {
--- backend/dscatalog/src/main/java/com/devsuperior/curso/config/SecurityConfiguration.java
+++ backend/dscatalog/src/main/java/com/devsuperior/curso/config/SecurityConfiguration.java
@@ -2,8 +2,6 @@ package com.devsuperior.curso.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 
 @Configuration
@@ -15,9 +13,6 @@ public class SecurityConfiguration {
 		return (web) -> web.ignoring().antMatchers("/actuator/**");
 	}
 	
-	@Bean
-	AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
-		return authConfig.getAuthenticationManager();
-	}
+	
 
 }
