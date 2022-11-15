609bfd1972a4d78cc12ea0fa5e7fb945afcecd92
Juan Nobert <nobert407@gmail.com>
2022-11-14 22:50:47 -0300

    Prettier config

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,9 +1,7 @@
-import './App.css';
-import './assets/styles/custom.scss';
+import "./App.css";
+import "./assets/styles/custom.scss";
 function App() {
-  return (
-    <h1>Hello DsCatalog</h1>
-  );
+  return <h1>Hello DsCatalog</h1>;
 }
 
 export default App;
