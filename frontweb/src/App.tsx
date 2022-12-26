5921b0e79b68533ecea75ef410b529f1190e7a23
Juan Nobert <nobert407@gmail.com>
2022-12-26 10:59:01 -0300

    Navbar styling

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,11 +1,12 @@
-import NavBar from "components/Navbar";
-import "./App.css";
 import "./assets/styles/custom.scss";
+import './App.css';
+import NavBar from "components/Navbar";
 const App = () => {
   return (
     <>
     <NavBar/>
-    <h1>Hello DsCatalog</h1>;
+    <h1>Hello DsCatalog</h1>
+    <a href="htt">Teste</a>
     </>
   )
 };
